4608cad1b4e1b4246680bf700bc48c1e0d710db5
Gustavo Bosco <gustavo-bosco@hotmail.com>
2024-05-29 19:02:27 -0300

    Tentando buscar os dados recebidos.........

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
+import json
 from flask import Flask, request, jsonify, abort
 
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    print('X-Kommo-Secret:::', request.headers.get('X-Kommo-Secret'))
+
 
     # Logar os headers da requisição
     print("Headers da Requisição:", request.headers)
@@ -13,10 +14,15 @@ def webhook():
     # Logar o corpo da requisição
     print("Corpo da Requisição:", request.data)
 
-    # Obter os dados do JSON enviado pelo Kommo CRM
-    data = request.json
-    
-    # Logar os dados recebidos para ver no terminal ou logs do Heroku
-    print("Dados recebidos:", data)
+    # Tentar carregar o JSON manualmente
+    try:
+        data = request.get_json(force=True)
+        print("Dados recebidos:", data)
+    except Exception as e:
+        data = None
+        print("Erro ao decodificar JSON:", e)
 
     return jsonify({'status': 'success'}), 200
+
+if __name__ == '__main__':
+    app.run()
