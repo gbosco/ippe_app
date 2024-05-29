aade91651e67d63cfddf1a5b196c9e3958e94896
Gustavo Bosco <gustavo-bosco@hotmail.com>
2024-05-29 17:42:47 -0300

    Alterado rota

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json
+    # Processar o webhook recebido
+    print(data)
+    return jsonify({'status': 'success'}), 200
 
 if __name__ == '__main__':
     app.run()
