177cb0710d175ab3f469a27a5a5e1cb5c65178aa
Gustavo Bosco <gustavo-bosco@hotmail.com>
2024-06-04 15:13:33 -0300

    testando as var de amb]

--- app.py
+++ app.py
@@ -47,7 +47,9 @@ def home():
     #auth_url = f"https://marceloluizpereira.kommo.com/oauth?client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&response_type=code&state=YOUR_STATE"
     #https://kommo.com/oauth/?client_id=e921a2d5-d899-4252-9ae4-4fa11740eceb&state=1&mode=post_message&redirect_uri=https://crm-ippe-6ccc57521801.herokuapp.com/auth
     auth_url = f"https://www.kommo.com/oauth/?client_id={CLIENT_ID}&state=1&mode=post_message&redirect_uri={REDIRECT_URI}&response_type=code"
-    return redirect(auth_url)
+    print('auth_url *---> ', auth_url)
+    #return redirect(auth_url)
+    return CLIENT_ID + '###' + CLIENT_SECRET
     
 
 @app.route('/auth')
